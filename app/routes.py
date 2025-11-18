d6171c1d8d1e86b85bc700460315d1ea3477fadc
jhnl18 <jhunelpenaflorida@gmail.com>
2025-11-18 17:36:59 +0800

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+from .decorators import login_required, role_required
+
+bp = Blueprint("main", __name__)
+
+@bp.route("/")
+def index():
+    return render_template("home.html")
+
+@bp.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+@bp.route("/admin")
+@login_required
+@role_required("admin")
+def admin_area():
+    return "Admin page only"
